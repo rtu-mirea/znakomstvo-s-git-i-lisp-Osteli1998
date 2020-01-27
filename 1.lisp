c042cd6286eb8cdb8b731cc5af49140e1a4dc398
Osteli1998 <60349293+Osteli1998@users.noreply.github.com>
2020-01-27 17:31:07 +0300

    Create 1.lisp

---
+++ 1.lisp
@@ -0,0 +1,10 @@
+;; Задание 1. Факториал и его вывод
+(defun factorial(n)
+    (if (= n 0)
+     1
+     (* n (factorial(- n 1)))))
+(loop for i from 0 to 16
+      do (format t "~D! = ~D~%" i (factorial i) ))
+(loop for i from 1 to 16
+      do (format t "~D,"(factorial i) )
+      finally (format t "..~%"))
